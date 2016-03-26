8789c2f53db5d6f94765e2bec31c22d698ec5569
s0108 <iamlockelightning@gmail.com>
2016-03-26 13:03:16 +0800

    Changed setting.json

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -40,3 +40,22 @@ Weibo.init(setting);
 $('#signin').click(function(){
   window.open(Weibo.authorize());
 });
+
+// var jsonParas = {
+// 	code:"the value of your browser's parameter code",
+// 	grant_type:"authorization_code"
+// };
+// Weibo.OAuth2.access_token(jsonParas,function(data){
+// 	console.log(data);
+// });
+
+// // set parameters
+// var para = {
+//     "source": Weibo.appKey.appKey,
+//     "access_token": 'your access_token here'
+// };
+
+// // get public timeline
+// Weibo.Statuses.public_timeline(para, function(data){
+//     console.log(data);
+// });
