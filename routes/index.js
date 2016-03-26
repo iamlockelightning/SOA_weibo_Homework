a55e2bdcb511090d1146e2f98208beac5a71c984
s0108 <iamlockelightning@gmail.com>
2016-03-27 01:59:03 +0800

    Remain 5->100, Emotion

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,11 @@ var setting = require('./setting.json');
 var JSON = require('JSON');
 Weibo.init(setting);
 
+var user_access_token;
+var user_uid;
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // console.log(req.params.id);
   res.render('index', { title: 'iamlockelightning' });
 });
 
@@ -16,21 +19,67 @@ router.get('/authorize', function(req, res, next){
 
 /* GET show page. */
 router.get('/show', function(req, res, next) {
-  ret_code = req.query.code;
-  console.log(ret_code);
-  console.log(Weibo.appKey);
+  // console.log(ret_code);
+  // console.log(Weibo.appKey);
   var jsonParas = {
-    client_id:Weibo.appKey.appKey,
-    client_secret:Weibo.appKey.appSecret,
-    grant_type:"authorization_code",
-    code: ret_code,//"the value of your browser's parameter code",
-    redirect_uri:Weibo.appKey.redirectUrl
+    client_id: Weibo.appKey.appKey,
+    client_secret: Weibo.appKey.appSecret,
+    grant_type: "authorization_code",
+    code: req.query.code,//"the value of your browser's parameter code",
+    redirect_uri: Weibo.appKey.redirectUrl
   };
   Weibo.OAuth2.access_token(jsonParas,function(data){
-    console.log(data);
-
+    user_access_token = data.access_token;
+    user_uid = data.uid;
+    // console.log(user_uid);
     res.render('show', { title: 'iamlockelightning', data : JSON.stringify(data)});
   });
+});
+
+/* GET post page. */
+router.get('/posts/:id', function(req, res, next) {
+  var id = req.params.id;
+  var para = {
+      access_token: user_access_token,
+      uid: id,
+      count: 100
+  };
+  Weibo.Statuses.user_timeline(para, function(timeline){
+      // console.log(timeline);
+      res.render('show', { title: 'iamlockelightning', data : JSON.stringify(timeline)});
+  });
+});
+
+/* GET emotion page. */
+router.get('/emotion/:id', function(req, res, next) {
+  var id = req.params.id;
+  var para = {
+      access_token: user_access_token,
+      uid: id,
+      count: 100
+  };
+  Weibo.Statuses.user_timeline(para, function(timeline){
+      // console.log("---------------");
+      // console.log(timeline);
+      var statuses = timeline.statuses;
+      var text_list = [];
+      statuses.forEach(function(e){
+        // console.log(e.text);
+        text_list.push(e.text);
+      });
+      res.render('show', { title: 'iamlockelightning', data : JSON.stringify(text_list)});
+  });
+});
+
+/* GET users page. */
+router.get('/users', function(req, res, next) {
+  var para = {
+      access_token: user_access_token,
+      uid: user_uid
+  };
+  Weibo.Users.show(para, function(info){
+    res.render('show', { title: 'iamlockelightning', data : JSON.stringify(info)});
+  })
 });
 
 module.exports = router;
