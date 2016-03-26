8eba6614d191d3d018dfd3691a3035612d16eb99
s0108 <iamlockelightning@gmail.com>
2016-03-26 15:41:38 +0800

    Be able to get Access_Token

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,61 +1,6 @@
 
-Weibo = {};
-    // must initialize before using Weibo Object
-    Weibo.init = function(setting) {
-      Weibo.appKey = {
-        "appKey":setting.appKey,
-        "appSecret":setting.appSecret,
-        "redirectUrl":setting.redirectUrl
-      };
-    };
-
-  Weibo.getGetURL = function(paras){
-    var arr = [];
-    for(var key in paras){
-      arr.push(key + '=' + paras[key]);
-    }
-    var path = '?client_id=' + Weibo.appKey.appKey + '&redirect_uri=' + Weibo.appKey.redirectUrl
-               + '&client_secret=' + Weibo.appKey.appSecret;
-    if(arr)
-      return path + '&' + arr.join('&');
-    return path + arr.join('&');
-  };
-
-  Weibo.getPostURL = function(paras){
-    if(!paras)
-      paras = {};
-    paras.client_id = Weibo.appKey.appKey;
-    paras.redirect_uri = Weibo.appKey.redirectUrl;
-    paras.client_secret = Weibo.appKey.appSecret;
-    return paras;
-  };
-
-    Weibo.authorize = function(){
-        var path = 'https://api.weibo.com/oauth2/authorize' + Weibo.getGetURL();
-        return path;
-    };
-
-console.log(setting)
-Weibo.init(setting);
 $('#signin').click(function(){
-  window.open(Weibo.authorize());
+  $.get('authorize', function(url){
+    window.open(url);
+  });
 });
-
-// var jsonParas = {
-// 	code:"the value of your browser's parameter code",
-// 	grant_type:"authorization_code"
-// };
-// Weibo.OAuth2.access_token(jsonParas,function(data){
-// 	console.log(data);
-// });
-
-// // set parameters
-// var para = {
-//     "source": Weibo.appKey.appKey,
-//     "access_token": 'your access_token here'
-// };
-
-// // get public timeline
-// Weibo.Statuses.public_timeline(para, function(data){
-//     console.log(data);
-// });
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var Weibo = require('nodeweibo');
+var setting = require('./setting.json');
+var JSON = require('JSON');
+Weibo.init(setting);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'iamlockelightning' });
 });
 
+router.get('/authorize', function(req, res, next){
+  req.send(Weibo.authorize());
+});
+
 /* GET show page. */
 router.get('/show', function(req, res, next) {
-  res.render('show', { title: 'iamlockelightning' });
+  ret_code = req.query.code;
+  console.log(ret_code);
+  console.log(Weibo.appKey);
+  var jsonParas = {
+    client_id:Weibo.appKey.appKey,
+    client_secret:Weibo.appKey.appSecret,
+    grant_type:"authorization_code",
+    code: ret_code,//"the value of your browser's parameter code",
+    redirect_uri:Weibo.appKey.redirectUrl
+  };
+  Weibo.OAuth2.access_token(jsonParas,function(data){
+    console.log(data);
+
+    res.render('show', { title: 'iamlockelightning', data : JSON.stringify(data)});
+  });
 });
 
 module.exports = router;
