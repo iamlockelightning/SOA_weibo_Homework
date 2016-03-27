002c067c36e7fef38f70cdf43100dccee796e94a
s0108 <iamlockelightning@gmail.com>
2016-03-28 00:06:44 +0800

    Almost done!!!

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Weibo = require('nodeweibo');
+var http = require('http');
 var setting = require('./setting.json');
 var JSON = require('JSON');
 Weibo.init(setting);
@@ -42,12 +43,44 @@ router.get('/posts/:id', function(req, res, next) {
   var para = {
       access_token: user_access_token,
       uid: id,
-      count: 100
+      count: 100,
+      max_id: 0
   };
-  Weibo.Statuses.user_timeline(para, function(timeline){
-      // console.log(timeline);
-      res.render('show', { title: 'iamlockelightning', data : JSON.stringify(timeline)});
-  });
+
+  // Weibo.Statuses.user_timeline(para, function(timeline){
+  //     // console.log(timeline);
+  //     var statuses = timeline.statuses;
+  //     var text_list = [];
+  //     statuses.forEach(function(e){
+  //       // console.log(e.text);
+  //       text_list.push(e.text);
+  //     });
+  //     res.render('show', { title: 'iamlockelightning', data : JSON.stringify(timeline)});
+  // });
+
+  var statuses_list = [];
+  var new_id;
+  var count = 20;
+  var finish = function(timeline) {
+    // console.log(count);
+    var statuses = timeline.statuses;
+    statuses.forEach(function(e){
+      // console.log(e.id);
+      new_id = e.id;
+      statuses_list.push(e);
+    });
+    para['max_id'] = new_id;
+    count--;
+    if(count > 0){
+      Weibo.Statuses.user_timeline(para, finish);
+    }
+    if(count == 0){
+      res.render('show', { title: 'iamlockelightning', data: JSON.stringify(statuses_list)});
+    }
+  };
+  Weibo.Statuses.user_timeline(para, finish);
+  // console.log(statuses_list);
+  // res.render('show', { title: 'iamlockelightning', data: JSON.stringify(statuses_list)});
 });
 
 /* GET emotion page. */
@@ -58,16 +91,54 @@ router.get('/emotion/:id', function(req, res, next) {
       uid: id,
       count: 100
   };
+  var infoo = "JSON 格式的 [double, double] 类型组成的列表。每个元素分别表示请求的列表对应位置的文本的情感判断结果；第一个值为非负面概率，第二个值为负面概率，两个值相加和为 1。";
   Weibo.Statuses.user_timeline(para, function(timeline){
       // console.log("---------------");
       // console.log(timeline);
       var statuses = timeline.statuses;
       var text_list = [];
+      // var word_list = [];
+      var word_list = '';
       statuses.forEach(function(e){
         // console.log(e.text);
         text_list.push(e.text);
+        var patt = /\[[^\]]+\]/g;
+        e.text.match(patt).forEach(function(w){
+          word_list += w;
+        });
       });
-      res.render('show', { title: 'iamlockelightning', data : JSON.stringify(text_list)});
+
+      var opt = {
+        url: 'http://api.bosonnlp.com/sentiment/analysis?weibo',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'X-Token': 'EGO3bf5q.5590.K4UZUWYVnTIQ'
+        }
+      };
+      // console.log(word_list);
+      // text_list = JSON.stringify(text_list);
+      word_list = JSON.stringify(word_list);
+      console.log(word_list);
+
+      var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "http://api.bosonnlp.com/sentiment/analysis?weibo", true);
+      xhr.setRequestHeader("Content-type","application/json");
+      xhr.setRequestHeader("Accept", "application/json");
+      xhr.setRequestHeader("X-Token", "EGO3bf5q.5590.K4UZUWYVnTIQ");
+      xhr.onreadystatechange = function(){
+          var XMLHttpReq = xhr;
+          if (XMLHttpReq.readyState == 4) {
+              if (XMLHttpReq.status == 200) {
+                  var text = XMLHttpReq.responseText;
+                  console.log(text);
+                  res.render('show', { title: 'iamlockelightning', info: infoo, data : JSON.stringify(text)});
+              }
+          }
+      };
+      xhr.send(word_list);
   });
 });
 
